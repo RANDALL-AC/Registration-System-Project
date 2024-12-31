ebbb8e8ef9cf2a912e8b086d32e4f1b051b125c1
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 13:38:07 -0600

    Implement constructors and destructor for Course class

--- Project I - Enrollment System/Course.cpp
+++ Project I - Enrollment System/Course.cpp
@@ -1,1 +1,17 @@
 #include "Course.h"
+
+Course::Course() {
+    this->courseId = "";
+    this->courseName = "";
+    this->credits = 0;
+    this->assignedProfessor = "";
+}
+
+Course::Course(std::string courseId, std::string courseName, int credits, std::string assignedProfessor) {
+    this->courseId = courseId;
+    this->courseName = courseName;
+    this->credits = credits;
+    this->assignedProfessor = assignedProfessor;
+}
+
+Course::~Course() {}
