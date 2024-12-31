3ce3a8b5d78e6015a0b33f08f5c4e76cf6556e12
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 14:41:46 -0600

    Implement constructors and destructor for Registration class

--- Project I - Enrollment System/Registration.cpp
+++ Project I - Enrollment System/Registration.cpp
@@ -1,1 +1,11 @@
 #include "Registration.h"
+
+Registration::Registration() {}
+
+Registration::Registration(Student student, Course course, Schedule schedule) {
+    this->student = student;
+    this->course = course;
+    this->schedule = schedule;
+}
+
+Registration::~Registration() {};
