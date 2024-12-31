215feb046a16b788a593492b8ece58b26ac1ba36
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 14:04:22 -0600

    Define the structure of the Schedule class with basic attributes and methods (day, startTime, endTime, classroom)

--- Project I - Enrollment System/Schedule.h
+++ Project I - Enrollment System/Schedule.h
@@ -5,6 +5,26 @@
 
 class Schedule
 {
+public:
 
+    Schedule();
+    Schedule(std::string day, std::string startTime, std::string endTime, std::string classroom, std::string courseName);
+    ~Schedule();
+
+    std::string getDay();
+    std::string getStartTime();
+    std::string getEndTime();
+    std::string getClassroom();
+
+    void setDay(std::string day);
+    void setStartTime(std::string startTime);
+    void setEndTime(std::string endTime);
+    void setClassroom(std::string classroom);
+
+private:
+    std::string day;
+    std::string startTime;
+    std::string endTime;
+    std::string classroom;
 };
 #endif
