ed0fd98ed03c44ec0e0b2bd6af5e6a74867aa3ac
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 14:36:22 -0600

    Add Registration class definition with renamed variables (student, course, schedule)

--- Project I - Enrollment System/Registration.h
+++ Project I - Enrollment System/Registration.h
@@ -2,8 +2,28 @@
 #ifndef REGISTRATION_H
 #define REGISTRATION_H
 #include <iostream>
+#include "Student.h"
+#include "Course.h"
+#include "Schedule.h"
 
 class Registration
 {
+    Registration();
+    Registration(Student student, Course course, Schedule schedule);
+    ~Registration();
+
+    Student getStudent();
+    Course getCourse();
+    Schedule getSchedule();
+
+    void setStudent(Student student);
+    void setCourse(Course course);
+    void setSchedule(Schedule schedule);
+
+
+private:
+    Student student;
+    Course course;
+    Schedule schedule;
 };
 #endif
