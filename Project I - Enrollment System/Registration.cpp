a7d506e0b0846230f9e083d36360ef8e9ef551a7
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 14:48:39 -0600

    Add getters and setters for Registration class attributes

--- Project I - Enrollment System/Registration.cpp
+++ Project I - Enrollment System/Registration.cpp
@@ -9,3 +9,27 @@ Registration::Registration(Student student, Course course, Schedule schedule) {
 }
 
 Registration::~Registration() {};
+
+Student Registration::getStudent() {
+    return this->student;
+}
+
+Course Registration::getCourse() {
+    return this->course;
+}
+
+Schedule Registration::getSchedule() {
+    return this->schedule;
+}
+
+void Registration::setStudent(Student student) {
+    this->student = student;
+}
+
+void Registration::setCourse(Course course) {
+    this->course = course;
+}
+
+void Registration::setSchedule(Schedule schedule) {
+    this->schedule = schedule;
+}
