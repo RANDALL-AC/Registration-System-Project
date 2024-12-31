f56a8a921a317359a93048871dc1d55aca40c7a3
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 12:34:34 -0600

    Implement constructors and destructor for Student class

--- Project I - Enrollment System/Student.cpp
+++ Project I - Enrollment System/Student.cpp
@@ -1,1 +1,17 @@
 #include "Student.h"
+
+Student::Student() {
+    this->name = "";
+    this->id = "";
+    this->career = "";
+    this->level = 0;
+}
+
+Student::Student(std::string name, std::string id, std::string career, int level) {
+    this->name = name;
+    this->id = id;
+    this->career = career;
+    this->level = level;
+}
+
+Student::~Student() {}
