5218c354094cde6763fa61c0da1645dadb62ee8e
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 13:21:32 -0600

    Define the structure of the Course class with basic attributes and methods (courseName, courseId, credits, assignedProfessor)

--- Project I - Enrollment System/Course.h
+++ Project I - Enrollment System/Course.h
@@ -1,8 +1,30 @@
 #pragma once
 #ifndef COURSE_H
 #define COURSE_H
+#include <iostream>
+
 class Course
 {
+public:
 
+    Course();
+    Course(std::string courseName, std::string courseId, int credits, std::string assignedProfessor);
+    ~Course();
+
+    std::string getCourseName();
+    std::string getCourseId();
+    int getCredits() const;
+    std::string getAssignedProfessor();
+
+    void setCourseName(std::string courseName);
+    void setCourseId(std::string courseId);
+    void setCredits(int credits);
+    void setAssignedProfessor(std::string assignedProfessor);
+
+private:
+    std::string courseName;
+    std::string courseId;
+    int credits;
+    std::string assignedProfessor;
 };
 #endif
