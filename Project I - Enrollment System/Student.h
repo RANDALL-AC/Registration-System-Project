5a6d00a78252d0397fd872d3fa22185d769e43bb
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 12:31:56 -0600

    Define the structure of the Student class with basic attributes and methods (name, id, career, level)

--- Project I - Enrollment System/Student.h
+++ Project I - Enrollment System/Student.h
@@ -4,6 +4,24 @@
 #include <iostream>
 class Student
 {
+public:
+    Student();
+    Student(std::string name, std::string id, std::string career, int level);
+    ~Student();
 
+    std::string getName();
+    std::string getId();
+    std::string getCareer();
+    int getLevel() const;
+
+    void setName(std::string name);
+    void setId(std::string id);
+    void setCareer(std::string career);
+    void setLevel(int level);
+private:
+    std::string name;
+    std::string id;
+    std::string career;
+    int level;
 };
 #endif
