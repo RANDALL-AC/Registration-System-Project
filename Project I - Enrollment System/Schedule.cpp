cac9fac84908d6b48691934685d3170bf9aa5afa
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 14:17:19 -0600

    Implement constructors and destructor for Schedule class

--- Project I - Enrollment System/Schedule.cpp
+++ Project I - Enrollment System/Schedule.cpp
@@ -1,1 +1,17 @@
 #include "Schedule.h"
+
+Schedule::Schedule() {
+    this->day = "";
+    this->startTime = "";
+    this->endTime = "";
+    this->classroom = "";
+}
+
+Schedule::Schedule(std::string day, std::string startTime, std::string endTime, std::string classroom, std::string courseName) {
+    this->day = day;
+    this->startTime = startTime;
+    this->endTime = endTime;
+    this->classroom = classroom;
+}
+
+Schedule::~Schedule() {}
