ff220e108b1d688402ee7f0255f2d81da998cb21
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 12:44:21 -0600

    Add getters and setters for Student class attributes

--- Project I - Enrollment System/Student.cpp
+++ Project I - Enrollment System/Student.cpp
@@ -15,3 +15,35 @@ Student::Student(std::string name, std::string id, std::string career, int level
 }
 
 Student::~Student() {}
+
+std::string Student::getName() {
+    return this->name;
+}
+
+std::string Student::getId() {
+    return this->id;
+}
+
+std::string Student::getCareer() {
+    return this->career;
+}
+
+int Student::getLevel() const {
+    return this->level;
+}
+
+void Student::setName(std::string name) {
+    this->name = name;
+}
+
+void Student::setId(std::string id) {
+    this->id = id;
+}
+
+void Student::setCareer(std::string career) {
+    this->career = career;
+}
+
+void Student::setLevel(int level) {
+    this->level = level;
+}
