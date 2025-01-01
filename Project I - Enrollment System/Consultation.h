09a4a4f7396200425e5e5d38cf6172eafc9402cf
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 20:35:29 -0600

    Add the Consultation class

---
+++ Project I - Enrollment System/Consultation.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef CONSULTATION_H
+#define CONSULTATION_H
+
+#include <iostream>
+#include "student.h"
+#include "course.h"
+#include "schedule.h"
+#include "registration.h"
+
+class Consultation
+{
+};
+#endif
