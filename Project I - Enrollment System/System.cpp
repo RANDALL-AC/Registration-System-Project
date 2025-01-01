1fa6c4da78f951e342466e2801b5abe025431122
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 22:13:54 -0600

    Implemented the 'showFileMenu' function to display a menu with the 'about' and 'exitSystem

--- Project I - Enrollment System/System.cpp
+++ Project I - Enrollment System/System.cpp
@@ -7,6 +7,31 @@ System::System() {
     numRegistrations = 0;
 }
 
+void System::showFileMenu() {
+    int option;
+    do {
+        std::cout << "\n--- Menu de Archivo ---\n";
+        std::cout << "1. Acerca de..\n";
+        std::cout << "2. Volver\n";
+        std::cout << "3. Salir del Sistema\n";
+        std::cout << "Seleccione una opcion: ";
+        std::cin >> option;
+
+        switch (option) {
+        case 1:
+            about();
+            break;
+        case 2:
+            return;
+        case 3:
+            exitSystem();
+            break;
+        default:
+            std::cout << "Opcion invalida. Intente de nuevo.\n";
+        }
+    } while (true); 
+}
+
 void System::about() {
     std::cout << "\nEste sistema de matricula fue desarrollado por [ Inge. RANDALL ARAUZ CUBILLA ].\n";
     std::cout << "Para mas informacion o consultas al numero: 27325614 o 88125674.\n";
