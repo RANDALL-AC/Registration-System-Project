0e7ff3649d4db55a23c5735b7156a9b39f17d12c
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 20:15:40 -0600

    Define static arrays to manage students, courses, schedules, and registrations.

--- Project I - Enrollment System/System.h
+++ Project I - Enrollment System/System.h
@@ -8,6 +8,11 @@
 #include "schedule.h"
 #include "registration.h"
 
+const int maxStudents = 20;
+const int maxCourses = 10;
+const int maxSchedules = 10;
+const int maxCoursesPerStudents = 4;
+
 class System
 {
 public:
@@ -31,5 +36,14 @@ public:
 
 private:
 
+    int numStudents = 0;
+    int numCourses = 0;
+    int numSchedules = 0;
+    int numRegistrations = 0;
+
+    Student students[maxStudents];
+    Course courses[maxCourses];
+    Schedule schedules[maxSchedules];
+    Registration registrations[maxStudents * maxCoursesPerStudents];
 };
 #endif 
