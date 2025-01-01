057ae9d3c7721589b1069b9a75a4646c048f1acc
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 22:00:52 -0600

    Implemented the 'about()' function to display information about the program.

--- Project I - Enrollment System/System.cpp
+++ Project I - Enrollment System/System.cpp
@@ -6,3 +6,8 @@ System::System() {
     numSchedules = 0;
     numRegistrations = 0;
 }
+
+void System::about() {
+    std::cout << "\nEste sistema de matricula fue desarrollado por [ Inge. RANDALL ARAUZ CUBILLA ].\n";
+    std::cout << "Para mas informacion o consultas al numero: 27325614 o 88125674.\n";
+}
