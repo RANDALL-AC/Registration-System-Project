10c46c42e8571a6a3e38f62286b409ae9d4f3dda
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-01-01 15:01:29 -0600

    Implemented the showCourseSchedule function to display the schedule of a course, including day, classroom, start time, and end time for each session.

--- Project I - Enrollment System/Consultation.cpp
+++ Project I - Enrollment System/Consultation.cpp
@@ -14,3 +14,14 @@ void Consultation::showRegisteredStudent(Student student, int courseCount) {
         std::cout << "El estudiante no tiene cursos matriculados.\n";
     }
 }
+
+void Consultation::showCourseSchedule(Course course) {
+    std::cout << "Horarios del curso de " << course.getCourseName() << ": ";
+    for (int i = 0; i < course.getNumSchedule(); ++i) {
+        Schedule schedule = course.getSchedule(i);
+        std::cout << "( Dia: " << schedule.getDay()
+            << ", Aula: " << schedule.getClassroom()
+            << ", Inicio: " << schedule.getStartTime()
+            << ", Fin: " << schedule.getEndTime() << " )\n\n";
+    }
+}
