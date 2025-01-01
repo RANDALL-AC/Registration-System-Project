14851e91d9bad052e114b086bcd03cd634da89c5
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2024-12-31 20:13:16 -0600

    Add main system functions, such as displaying menus, registering enrollments, students, courses, schedules, and checking schedule conflicts.

--- Project I - Enrollment System/System.h
+++ Project I - Enrollment System/System.h
@@ -1,9 +1,35 @@
 #pragma once
 #ifndef SYSTEM_H
 #define SYSTEM_H
+
 #include <iostream>
+#include "student.h"
+#include "course.h"
+#include "schedule.h"
+#include "registration.h"
 
 class System
 {
+public:
+
+    System();
+
+    void showMenu();
+
+    void showFileMenu();
+    void about();
+    void exitSystem(); 
+
+    void showMaintenanceMenu(); 
+    void addStudent();
+    void addCourse();
+    void addSchedule();
+
+    void showRegistrationMenu(); 
+    void registerStudent();
+    bool checkScheduleConflict(Student newStudent, Schedule newSchedule); 
+
+private:
+
 };
 #endif 
