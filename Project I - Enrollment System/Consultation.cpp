a88cca432ddcd6ea898276686d21c97e9a1eabb2
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-01-01 14:54:44 -0600

    Implemented the showRegisteredStudent function to display personal information of registered students.

--- Project I - Enrollment System/Consultation.cpp
+++ Project I - Enrollment System/Consultation.cpp
@@ -1,1 +1,16 @@
 #include "Consultation.h"
+
+void Consultation::showRegisteredStudent(Student student, int courseCount) {
+    if (courseCount > 0) {
+        std::cout << "\n-------------------------------------------------------------------------------------------------------------\n";
+        printf("\nDatos del estudiante matriculado:\n");
+        std::cout << "Nombre: " << student.getName()
+            << ", Cedula: " << student.getId()
+            << ", Carrera: " << student.getCareer()
+            << ", Nivel: " << student.getLevel();
+        std::cout << "\n-------------------------------------------------------------------------------------------------------------\n";
+    }
+    else {
+        std::cout << "El estudiante no tiene cursos matriculados.\n";
+    }
+}
